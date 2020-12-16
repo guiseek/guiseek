a42a93d8c8bc6822d71f4ea1930ea290f770592e
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-12-15 23:01:17 -0300

    chore: fix build script and remove dot env

--- apps/sample/gateway/src/app.module.ts
+++ apps/sample/gateway/src/app.module.ts
@@ -1,31 +1,12 @@
-import { W3Publication, W3PublicationSchema } from './w3/w3-publication.schema'
-import { ConfigModule, ConfigService } from '@nestjs/config'
-import { MongooseModule } from '@nestjs/mongoose'
 import { Module } from '@nestjs/common'
-
-import { PeerGatewayModule } from '@seek-peer/gateway'
-import { W3Controller } from './w3/w3.controller'
-import { AppController } from './app.controller'
 import { AppLogger } from './app.logger'
-import { W3Service } from './w3/w3.service'
-
-const mongooseFactory = (configService: ConfigService) => {
-  return { uri: configService.get('MONGO_URI') }
-}
+import { AppController } from './app.controller'
+import { PeerGatewayModule } from '@seek-peer/gateway';
+// import { PeerGateway } from './peer.gateway'
 
 @Module({
-  imports: [
-    PeerGatewayModule,
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: mongooseFactory,
-      inject: [ConfigService],
-    }),
-    MongooseModule.forFeature([
-      { name: W3Publication.name, schema: W3PublicationSchema },
-    ]),
-  ],
-  providers: [AppLogger, W3Service],
-  controllers: [AppController, W3Controller],
+  imports: [PeerGatewayModule],
+  providers: [AppLogger],
+  controllers: [AppController],
 })
 export class AppModule {}
